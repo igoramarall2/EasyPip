a6dc6a9b18d374ba42a3a8d36c968bef0f2928fc
igoramarall2 <119361388+igoramarall2@users.noreply.github.com>
2023-10-11 18:03:42 -0300

    Update easy_install.py

--- easy_install.py
+++ easy_install.py
@@ -61,143 +61,142 @@ def __init__(self):
 """
 Initializes the Pip instance. It sets the path to the Python executable and the module name.
 """
-module_name = bpy.context.scene.modules
-python_exe = os.path.join(sys.prefix, "bin", "python.exe")
-self.python = python_exe
-self.module_name = module_name
+  module_name = bpy.context.scene.modules
+  python_exe = os.path.join(sys.prefix, "bin", "python.exe")
+  self.python = python_exe
+  self.module_name = module_name
 
-def Upgrade_PIP(self):
-"""
-Upgrades the pip tool and ensures pip is installed.
+  def Upgrade_PIP(self):
+    """
+    Upgrades the pip tool and ensures pip is installed.
+    
+    Returns:
+    dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
+    """
+    subprocess.call([self.python, "-m", "ensurepip"])
+    subprocess.call([self.python, "-m", "pip", "install", "--upgrade", "pip"])
+    CaixaMensagemPM("Pip updated", "DONE", "DOCUMENTS")
+    return {"FINISHED"}
 
-Returns:
-dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
-"""
-subprocess.call([self.python, "-m", "ensurepip"])
-subprocess.call([self.python, "-m", "pip", "install", "--upgrade", "pip"])
-CaixaMensagemPM("Pip updated", "DONE", "DOCUMENTS")
-return {"FINISHED"}
+  def Install_Modules(self):
+    """
+    Installs a specified Python module using pip. Requires setting `self.module_name` first.
+    
+    Returns:
+    dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
+    """
+    subprocess.call([self.python, "-m", "pip", "install", self.module_name])
+    CaixaMensagemPM("Module installed. Please restart Blender", "DONE", "DOCUMENTS")
+    return {"FINISHED"}
 
-def Install_Modules(self):
-"""
-Installs a specified Python module using pip. Requires setting `self.module_name` first.
+    def Update_Modules(self):
+      """
+      Updates a specified Python module using pip. Requires setting `self.module_name` first.
+      
+      Returns:
+      dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
+      """
+      subprocess.call(
+      [self.python, "-m", "pip", "install", "--upgrade", self.module_name]
+      )
+      CaixaMensagemPM("Module updated. Please restart Blender", "DONE", "DOCUMENTS")
+      return {"FINISHED"}
 
-Returns:
-dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
-"""
-subprocess.call([self.python, "-m", "pip", "install", self.module_name])
-CaixaMensagemPM("Module installed. Please restart Blender", "DONE", "DOCUMENTS")
-return {"FINISHED"}
-
-def Update_Modules(self):
-"""
-Updates a specified Python module using pip. Requires setting `self.module_name` first.
-
-Returns:
-dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
-"""
-subprocess.call(
-[self.python, "-m", "pip", "install", "--upgrade", self.module_name]
-)
-CaixaMensagemPM("Module updated. Please restart Blender", "DONE", "DOCUMENTS")
-return {"FINISHED"}
-
-def Uninstall_Modules(self):
-"""
-Uninstalls a specified Python module using pip. Requires setting `self.module_name` first.
-
-Returns:
-dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
-"""
-subprocess.call([self.python, "-m", "pip", "uninstall", self.module_name])
-CaixaMensagemPM(
-"Module uninstalled. Please restart Blender",
-"DONE",
-"DOCUMENTS",
-)
-return {"FINISHED"}
+    def Uninstall_Modules(self):
+      """
+      Uninstalls a specified Python module using pip. Requires setting `self.module_name` first.
+      
+      Returns:
+      dict: A dictionary with the key "FINISHED" to indicate the operation is completed.
+      """
+      subprocess.call([self.python, "-m", "pip", "uninstall", self.module_name])
+      CaixaMensagemPM(
+      "Module uninstalled. Please restart Blender",
+      "DONE",
+      "DOCUMENTS",
+      )
+      return {"FINISHED"}
 
 
 class PIP_OT_tools(Operator):
-bl_idname = "pip.toolset"
-bl_label = "Ferramentas de Desenvolvedor"
-pip: bpy.props.EnumProperty(
-items=[
-("upgrade_pip", "upgrade_pip", "upgrade_pip"),
-("install_modules", "install_modules", "install_modules"),
-("upgrade_modules", "upgrade_modules", "upgrade_modules"),
-("uninstall_modules", "uninstall_modules", "uninstall_modules"),
-]
-)
+  bl_idname = "pip.toolset"
+  bl_label = "Ferramentas de Desenvolvedor"
+  pip: bpy.props.EnumProperty(
+  items=[
+  ("upgrade_pip", "upgrade_pip", "upgrade_pip"),
+  ("install_modules", "install_modules", "install_modules"),
+  ("upgrade_modules", "upgrade_modules", "upgrade_modules"),
+  ("uninstall_modules", "uninstall_modules", "uninstall_modules") ] )
 
-def execute(self, context):
-if self.pip == "upgrade_pip":
-Pip().Update_Modules()
-
-if self.pip == "install_modules":
-Pip().Install_Modules()
-
-if self.pip == "upgrade_modules":
-Pip().Update_Modules()
-
-if self.pip == "uninstall_modules":
-Pip().Update_Modules()
-return {"FINISHED"}
+  def execute(self, context):
+    if self.pip == "upgrade_pip":
+      Pip().Update_Modules()
+  
+    if self.pip == "install_modules":
+      Pip().Install_Modules()
+  
+    if self.pip == "upgrade_modules":
+      Pip().Update_Modules()
+  
+    if self.pip == "uninstall_modules":
+      Pip().Update_Modules()
+    
+    return {"FINISHED"}
 
 
 class PIP_PT_tools(Panel):
-"""Panel for module installation and PIP updates."""
-
-bl_label = "DEV: Install Modules"
-bl_space_type = "TEXT_EDITOR"
-bl_region_type = "UI"
-bl_category = "Install Modules"
-
-def draw(self, context):
-layout = self.layout
-box = layout.box()
-col = box.column()
-row = col.row(align=True)
-col.scale_y = 4
-col.separator(factor=1)
-row.prop(bpy.context.scene, "modules", text="", icon="FILE_SCRIPT")
-col.scale_y = 3
-row = col.row(align=True)
-row.operator(
-"pip.toolset", text="Install Module", icon="MODIFIER_ON"
-).pip = "install_modules"
-row = col.row(align=True)
-row.alert = True
-row.operator(
-"pip.toolset", text="Uninstall Module", icon="TRASH"
-).pip = "uninstall_modules"
-row = col.row(align=True)
-row.operator(
-"pip.toolset", text="Update PIP", icon="INDIRECT_ONLY_ON"
-).pip = "upgrade_pip"
-
-row.operator(
-"pip.toolset", text="Update Module", icon="INDIRECT_ONLY_ON"
-).pip = "upgrade_modules"
+  """Panel for module installation and PIP updates."""
+  
+  bl_label = "DEV: Install Modules"
+  bl_space_type = "TEXT_EDITOR"
+  bl_region_type = "UI"
+  bl_category = "Install Modules"
+  
+  def draw(self, context):
+    layout = self.layout
+    box = layout.box()
+    col = box.column()
+    row = col.row(align=True)
+    col.scale_y = 4
+    col.separator(factor=1)
+    row.prop(bpy.context.scene, "modules", text="", icon="FILE_SCRIPT")
+    col.scale_y = 3
+    row = col.row(align=True)
+    row.operator(
+    "pip.toolset", text="Install Module", icon="MODIFIER_ON"
+    ).pip = "install_modules"
+    row = col.row(align=True)
+    row.alert = True
+    row.operator(
+    "pip.toolset", text="Uninstall Module", icon="TRASH"
+    ).pip = "uninstall_modules"
+    row = col.row(align=True)
+    row.operator(
+    "pip.toolset", text="Update PIP", icon="INDIRECT_ONLY_ON"
+    ).pip = "upgrade_pip"
+    
+    row.operator(
+    "pip.toolset", text="Update Module", icon="INDIRECT_ONLY_ON"
+    ).pip = "upgrade_modules"
 
 
 classes = (PIP_OT_tools, PIP_PT_tools)
 
 
 def register():
-from bpy.utils import register_class
+  from bpy.utils import register_class
 
-for cls in classes:
-register_class(cls)
+  for cls in classes:
+    register_class(cls)
 
-bpy.types.Scene.modules = bpy.props.StringProperty(name="modulos", default="Modulo")
+    bpy.types.Scene.modules = bpy.props.StringProperty(name="modulos", default="Modulo")
 
 
 def unregister():
-from bpy.utils import unregister_class
+  from bpy.utils import unregister_class
 
-for cls in reversed(classes):
-unregister_class(cls)
+  for cls in reversed(classes):
+    unregister_class(cls)
 
 
 if __name__ == "__main__":
